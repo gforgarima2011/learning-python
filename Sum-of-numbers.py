0d34f22ec8f1b30c5da7ea26f055e8b1d68d309c
Dinker Pandey <dinker.pandey2010@gmail.com>
2023-11-16 11:03:27 +0530

    first commit

---
+++ Sum-of-numbers.py
@@ -0,0 +1,4 @@
+x = input("enter a number:")
+y=input("type another number :")
+sum = int(x) + int(y)
+print ("the sum is : ",sum)
---
+++ multiplication-of-numbers.py
@@ -0,0 +1,4 @@
+x=input("enter a number :")
+y=input ("enter another number :")
+sum = int (x) * int(y)
+print ("the multiply is : ",sum) 
